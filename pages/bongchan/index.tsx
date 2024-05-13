c7e558529d1018621cd461b1eb091ded1e57c6a9
devwqc <98067115+devwqc@users.noreply.github.com>
2024-05-13 23:11:14 +0900

    feat: Bongchan 페이지 SearchForm 추가

--- pages/bongchan/index.tsx
+++ pages/bongchan/index.tsx
@@ -1,5 +1,6 @@
 import FormBongchan from '@/components/bongchan/Form';
 import HookFormBongchan from '@/components/bongchan/HookForm';
+import SearchFormBongchan from '@/components/bongchan/SearchForm';
 
 export default function Bongchan() {
   return (
@@ -7,6 +8,10 @@ export default function Bongchan() {
       <div>
         <h2>일반 폼</h2>
         <FormBongchan />
+      </div>
+      <div>
+        <h2>검색 폼</h2>
+        <SearchFormBongchan />
       </div>
       <div>
         <h2>리액트 훅 폼</h2>
