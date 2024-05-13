9c6037d8d49993614999a4879e188080a4b2920e
devwqc <98067115+devwqc@users.noreply.github.com>
2024-05-14 00:44:36 +0900

    design: Bongchan 스타일링

--- pages/bongchan/index.tsx
+++ pages/bongchan/index.tsx
@@ -1,10 +1,15 @@
+import classNames from 'classnames/bind';
+
+import styles from './bongchan.module.scss';
 import FormBongchan from '@/components/bongchan/Form';
 import HookFormBongchan from '@/components/bongchan/HookForm';
 import SearchFormBongchan from '@/components/bongchan/SearchForm';
 
+const cx = classNames.bind(styles);
+
 export default function Bongchan() {
   return (
-    <div>
+    <div className={cx('page')}>
       <div>
         <h2>일반 폼</h2>
         <FormBongchan />
