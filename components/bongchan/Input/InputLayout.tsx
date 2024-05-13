202c48ef9c7a3525e47912fbe84b3a1831a712a4
devwqc <98067115+devwqc@users.noreply.github.com>
2024-05-13 23:21:10 +0900

    refactor: InputLayout className 적용

--- components/bongchan/Input/InputLayout.tsx
+++ components/bongchan/Input/InputLayout.tsx
@@ -1,5 +1,17 @@
-import { PropsWithChildren } from 'react';
+import classNames from 'classnames/bind';
+import { PropsWithChildren, ReactNode } from 'react';
 
-export default function InputLayoutBongchan({ children }: PropsWithChildren) {
-  return <div>{children}</div>;
+import styles from './InputBongchan.module.scss';
+
+const cx = classNames.bind(styles);
+
+interface InputLayoutBongchanProps extends PropsWithChildren {
+  className?: string;
+}
+
+export default function InputLayoutBongchan({
+  children,
+  className,
+}: InputLayoutBongchanProps) {
+  return <div className={cx('layout', className)}>{children}</div>;
 }
